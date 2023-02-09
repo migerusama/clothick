41d7454bf86be8406553857ddec5c4a1acb2e126
Miguel <migueldepablos99@gmail.com>
2023-02-09 01:13:45 +0100

    get products

--- home/home.js
+++ home/home.js
@@ -14,52 +14,100 @@ class Producto {
 $(function () {
     $("#nav-placeholder").load("../navbar/navbar.html");
 });
+var productos
+
+function getProducts() {
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            url: '../modelo/index.php',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                resolve(data);
+            },
+            error: function (xhr, status, error) {
+                reject(error);
+            }
+        });
+    });
+}
 
 var containerNovedades = document.getElementById("containerNovedades")
 var productosLista = document.getElementById("productosLista")
 
-var productosNovedades = [
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803")
-]
-var productos = [
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803"),
-    new Producto(0, "producto1", 10, "https://cdn.frankerfacez.com/avatar/twitch/244099803")
-]
-
-productosNovedades.forEach(producto => {
-    createProductCard(producto, containerNovedades, "col-4")
+getProducts().then(function (products) {
+    productos = products
+    productos.forEach(producto => {
+        containerNovedades.appendChild(createCard(producto, "m-2"))
+    });
+    productos.forEach(producto => {
+        productosLista.appendChild(createCard(producto))
+    })
+}, function (error) {
+    console.error(error);
 });
 
-productos.forEach(producto => {
-    createProductCard(producto, productosLista, "col-2")
-})
+function createCard(producto, margin = "") {
+    var card = document.createElement("div");
+    card.classList.add("col");
+    if (margin) card.classList.add(margin);
 
-function createProductCard(producto, container, col) {
-    var card = document.createElement("div")
-    card.classList.add("card", col, "bg-dark")
-    var img = document.createElement("img")
-    img.classList.add("card-img-top")
-    img.src = producto.img
-    var body = document.createElement("div")
-    body.classList.add("card-body")
-    var titulo = document.createElement("h5")
-    titulo.classList.add("card-tittle", "text-white")
-    titulo.innerText = producto.nombre
-    body.appendChild(titulo)
-    card.appendChild(img)
-    card.appendChild(body)
-    container.appendChild(card)
+    var innerCard = document.createElement("div");
+    innerCard.classList.add("card", "bg-dark");
+
+    var link = document.createElement("a");
+    link.href = "";
+
+    var img = document.createElement("img");
+    img.classList.add("card-img-top");
+    img.src = producto.image
+    link.appendChild(img);
+
+    var cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    var clearfix = document.createElement("div");
+    clearfix.classList.add("clearfix", "mb-3");
+
+    var badge = document.createElement("span");
+    badge.classList.add("float-start", "badge", "rounded-pill", "bg-primary");
+    badge.textContent = producto.name
+    clearfix.appendChild(badge);
+
+    var price = document.createElement("span");
+    price.classList.add("float-end", "price-hp");
+    price.innerHTML = `${producto.price}&euro;`
+    clearfix.appendChild(price);
+
+    cardBody.appendChild(clearfix);
+
+    var cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title", "text-white");
+    cardTitle.textContent = producto.description
+    cardBody.appendChild(cardTitle);
+
+    var textEnd = document.createElement("div");
+    textEnd.classList.add("text-end");
+
+    var buyBtn = document.createElement("button");
+    buyBtn.classList.add("btn", "btn-dark", "btn-outline-primary");
+    buyBtn.textContent = "BUY";
+    textEnd.appendChild(buyBtn);
+
+    //TODO: add event listeners
+
+    var checkBtn = document.createElement("button");
+    checkBtn.classList.add("btn", "btn-dark", "btn-outline-primary");
+    checkBtn.textContent = "CHECK";
+    textEnd.appendChild(checkBtn);
+
+    //TODO: add event listeners
+
+    cardBody.appendChild(textEnd);
+
+    innerCard.appendChild(link);
+    innerCard.appendChild(cardBody);
+    card.appendChild(innerCard);
+
+    return card;
 }
---
+++ modelo/index.php
@@ -0,0 +1,16 @@
+<?php
+$conn = new mysqli('localhost', 'localhost', '', 'clothick');
+
+$query = "SELECT * FROM products";
+$result = $conn->query($query);
+
+$products = array();
+
+while ($row = $result->fetch_assoc()) {
+    // Agrega cada producto como un objeto en el array
+    $products[] = (object) $row;
+}
+
+// Devuelve el array de productos en formato JSON
+header("Content-Type: application/json");
+echo json_encode($products);
